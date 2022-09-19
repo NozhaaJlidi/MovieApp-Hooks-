f6c41684572b23e4377aa5338c56691d2786da95
Nozha Jlidi <108417286+NozhaaJlidi@users.noreply.github.com>
2022-09-19 12:46:06 +0100

    first commit

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,25 @@
 import React, { useState } from "react";
 import AddMovie from "./AddMovie";
 import MovieCard from "./MovieCard";
-import { datalist } from "../data";
 import Button from "react-bootstrap/Button";
+import {useSelector} from "react-redux"
 
 const MovieList = ({ text }) => {
-  const [listToMap, setlistToMap] = useState(datalist);
+  const datalist= useSelector(state=>state.movie);
+  // const [listToMap, setlistToMap] = useState(datalist);
   const [showModal, setShowModal] = useState(false);
 
   return (
     <div className="container">
-      {listToMap
-      
-        .filter((el) => el ?.title?.toUpperCase().includes(text.toUpperCase()))
-        .map((el,i) => (
+      {datalist
+        .filter((el) => el?.title?.toUpperCase().includes(text.toUpperCase()))
+        .map((el, i) => (
           <MovieCard key={i} x={el} />
         ))}
       <Button onClick={() => setShowModal(true)} variant="outline-secondary">
         Add{" "}
       </Button>{" "}
-      {showModal && <AddMovie listToMap={listToMap} setlistToMap= {setlistToMap} setShowModal={setShowModal} /> }
+      {showModal && <AddMovie setShowModal={setShowModal} />}
     </div>
   );
 };
